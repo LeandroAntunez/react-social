48c330aa1ac5c0cbf46657b33030ec43b331b4c5
Leandro Antunez <leandro_antunez@outlook.com>
2022-04-06 21:00:32 -0300

    Change profile photo added, behavior is missing.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,11 +6,13 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router";
+import { AddAPhoto } from "@material-ui/icons";
 
 export default function Profile() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     const [user, setUser] = useState({});
     const username = useParams().username;
+    const [file, setFile] = useState(null);
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -19,6 +21,31 @@ export default function Profile() {
         };
         fetchUser();
     }, [username]);
+
+    const changeProfilePhoto = () => {
+        console.log(file)
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: user._id,
+        };
+        if (file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            newPost.img = fileName;
+            try {
+                await axios.post("/upload", data);
+            } catch (err) { }
+        }
+        try {
+            await axios.post("/posts", newPost);
+            window.location.reload();
+        } catch (err) { }
+    };
 
     return (
         <>
@@ -37,15 +64,32 @@ export default function Profile() {
                                 }
                                 alt=""
                             />
-                            <img
-                                className="profileUserImg"
-                                src={
-                                    user.profilePicture
-                                        ? PF + user.profilePicture
-                                        : PF + "person/noAvatar.png"
-                                }
-                                alt=""
-                            />
+                            <div className="profilePhotoSection">
+                                <img
+                                    className="profileUserImg"
+                                    src={
+                                        user.profilePicture
+                                            ? PF + user.profilePicture
+                                            : PF + "person/noAvatar.png"
+                                    }
+                                    alt=""
+                                />
+                                <form onSubmit={changeProfilePhoto()}>
+                                    <label htmlFor="file" >
+                                        <span className="profileChangeUserImg">
+                                            <AddAPhoto className="addAPhotoIcon" />
+                                            <input
+                                                style={{ display: "none" }}
+                                                type="file"
+                                                id="file"
+                                                accept=".png,.jpeg,.jpg"
+                                                onChange={(e) => setFile(e.target.files[0])}
+                                            />
+                                            <button type="submit">Aceptar</button>
+                                        </span>
+                                    </label>
+                                </form>
+                            </div>
                         </div>
                         <div className="profileInfo">
                             <h4 className="profileInfoName">{user.username}</h4>
