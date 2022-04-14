9dbf25fc2711d57af3e2ed459379f2b1465285f1
Leandro Antunez <leandro_antunez@outlook.com>
2022-04-13 21:33:51 -0300

    HomeRightbar component separated from Rightbar.

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,10 @@
 import "./rightbar.css";
-import { Users } from "../../dummyData";
-import Online from "../online/Online";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { Add, Remove } from "@material-ui/icons";
+import HomeRightbar from "../homeRightbar/HomeRightbar";
 
 export default function Rightbar({ user }) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -44,26 +43,6 @@ export default function Rightbar({ user }) {
         } catch (err) {
             console.log(err);
         }
-    };
-
-    const HomeRightbar = () => {
-        return (
-            <>
-                <div className="birthdayContainer">
-                    <img className="birthdayImg" src="assets/gift.png" alt="" />
-                    <span className="birthdayText">
-                        <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
-                    </span>
-                </div>
-                <img className="rightbarAd" src="assets/ad.png" alt="" />
-                <h4 className="rightbarTitle">Online Friends</h4>
-                <ul className="rightbarFriendList">
-                    {Users.map((u) => (
-                        <Online key={u.id} user={u} />
-                    ))}
-                </ul>
-            </>
-        );
     };
 
     const ProfileRightbar = () => {
