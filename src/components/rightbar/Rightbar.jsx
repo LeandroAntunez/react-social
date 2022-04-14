c8f74e9ae22cc325d1ff243c13c5043ed6c5545e
Leandro Antunez <leandro_antunez@outlook.com>
2022-04-13 21:22:40 -0300

    Rightbar warning message fixed.

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -25,7 +25,7 @@ export default function Rightbar({ user }) {
                 })
         };
         getFriends();
-    }, [user]);
+    }, [user, currentUser._id]);
 
     const handleClick = async () => {
         try {
